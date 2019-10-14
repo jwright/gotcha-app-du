6a873d875b22713d3f5d4ca00087f9c82eced6bc
Jamie Wright <jamie@brilliantfantastic.com>
2019-10-14 10:49:37 -0500

    Initial arenas query

--- src/screens/ArenaListScreen/index.js
+++ src/screens/ArenaListScreen/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
+import ArenasQuery from "../../queries/Arenas";
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
