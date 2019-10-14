5407441e4fa1502f15aee94b48886c44381ba82a
Jamie Wright <jamie@brilliantfantastic.com>
2019-10-14 10:54:06 -0500

    Initial client

---
+++ src/helpers/client.js
@@ -0,0 +1,13 @@
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    new HttpLink({ uri: "http://localhost:4000/graphql" })
+  ]),
+  cache: new InMemoryCache(),
+});
+
+export default client;
