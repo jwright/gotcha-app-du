7467cd8147590c6cf0cdd217fab76b91b486a705
Jamie Wright <jamie@brilliantfantastic.com>
2019-10-14 11:14:34 -0500

    Call the query

--- src/screens/ArenaListScreen/index.js
+++ src/screens/ArenaListScreen/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { useQuery } from "@apollo/react-hooks";
 
 import ArenasQuery from "../../queries/Arenas";
 
@@ -14,6 +15,15 @@ const styles = StyleSheet.create({
 
 const ArenaListScreen = ({ navigation }) => {
   const { longitude, latitude } = navigation.getParam("location");
+  const { loading, error, data } = useQuery(ArenasQuery, {
+    variables: {
+      latitude, longitude, radius: 25,
+    }
+  });
+
+  if (loading) { console.log("LOADING!"); }
+  if (error) { console.log("ERROR!", error); }
+  if (data) { console.log("DATA", data); }
 
   return (
     <View style={styles.container}>
